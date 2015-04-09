5dc9888d9c8e1310c8b15a78a2f0c64fbb1bceb1
jholcomb <jacob.r.holcomb@gmail.com>
2015-04-08 22:20:02 -0400

    Code is now segmented into nice methods.

--- Maincpp.cpp
+++ Maincpp.cpp
@@ -13,7 +13,10 @@ using namespace std;
 
 void initializeTree();
 void printIntro();
-void addPerson(map<string,Person>& familyTree, string key, Person& currentPerson);
+void addPersonToTree(map<string,Person>& familyTree, Person& currentPerson);
+bool isPersonFound(string selectedPerson, map<string,Person>& familyTree, map<string, Person>::iterator& it);
+void addRelative(map<string,Person>& familyTree);
+void connectRelative(string type, map<string,Person>& familyTree, Person& currentPerson);
 Person createRelative();
 
 int main(int argCount, char *argValues[]) {
@@ -25,7 +28,8 @@ int main(int argCount, char *argValues[]) {
 	printIntro();
 
 	it = familyTree.end();
-	currentCommand = " ";
+	currentCommand = "";
+
 	while (currentCommand != "Q") {
 		cout << endl << "What person would you like to modify (first name, last name, birthday)?"; 
 		getline(cin, selectedPerson);
@@ -38,46 +42,34 @@ int main(int argCount, char *argValues[]) {
 		// Case 1 - The Person was found and we continue about our way.
 		// Case 2 - The Person is not found and we provide an alternative creation method.
 
-		//Stage One (Case 2)
-		it = familyTree.find(selectedPerson);
-		if (it == familyTree.end()) {
-			cout << endl << "Person does not exist!!! Would you like to create them??? (y or n):";
-			getline(cin, response);
-			if (response == "y" || response == "Y") {
-				currentPerson = createRelative();
-				firstName = currentPerson.getFirstName();
-				lastName  = currentPerson.getLastName();
-				birthDate = currentPerson.getBirthDate();
-				familyTree[firstName + ", " + lastName + ", " + birthDate] = currentPerson;
-				currentCommand = "";
-			} else
-				//Restart the Loop, there was an error and the user did not wish to correct is
-				//by creating a new user
-				currentCommand = "";
-				continue;
-
 		//Stage One (Case 1)
-		} else {
-			currentPerson = it->second;
-			cout << endl << "Command> ";
-			getline(cin, currentCommand);
-		}
+		if (isPersonFound(selectedPerson, familyTree, it) == true) {
+			while (currentCommand == "") {
+				currentPerson = it->second;
+				cout << endl << "Command> ";
+				getline(cin, currentCommand);
 
-		if (currentCommand == "M") {
-			currentPerson.setMother(createRelative());
-		} else if (currentCommand == "F") {
-			currentPerson.setFather(createRelative());
-		} else if (currentCommand == "C") {
-			 currentPerson.addChild(createRelative());
-		} else if (currentCommand == "I") {
+				if (currentCommand == "M") {
+					connectRelative("Mother", familyTree, currentPerson);
+				} else if (currentCommand == "F") {
+					connectRelative("Father", familyTree, currentPerson);
+				} else if (currentCommand == "C") {
+					connectRelative("Child", familyTree, currentPerson);
+				} else if (currentCommand == "I") {
 
-		} else if (currentCommand == "D") {
+				} else if (currentCommand == "D") {
 		
-		} else if (currentCommand == "Q") {
-			cout << endl << "Family Tree Manager Shutting Down..." << endl;
-		} else
-			cout << endl << "Invalid Command" << endl;
-		}
+				} else if (currentCommand == "Q") {
+					cout << endl << "Family Tree Manager Shutting Down..." << endl;
+				} else {
+					cout << endl << "Invalid Command" << endl;
+					currentCommand = "";
+				}
+			}
+		//Stage One (Case 2)
+		} else 
+			addRelative(familyTree);
+	}
 	return EXIT_SUCCESS;
 }
 
@@ -93,7 +85,7 @@ void initializeTree() {
 void printIntro() {
 	cout << "Welcome to Jake's Family Tree Manager Version 1.0" << endl;
 	cout << endl << "The following commands can be used:" << endl;
-	cout << "*   M - Add a person's mother" << endl;
+	cout << endl << "*   M - Add a person's mother" << endl;
 	cout << "*   F - Add a person's father" << endl;
 	cout << "*   C - Add a person's child" << endl;
 	cout << "*   I - List immediate family (parents and siblings)" << endl;
@@ -101,8 +93,15 @@ void printIntro() {
 	cout << "*   Q - Quit the application" << endl;
 }
 
-void addPerson(map<string,Person>& familyTree, string key, Person& currentPerson) {
-	familyTree[key] = currentPerson;
+void addPersonToTree(map<string,Person>& familyTree, Person& currentPerson) {
+	string firstName, lastName, birthDate;
+	
+	firstName = currentPerson.getFirstName();
+	lastName  = currentPerson.getLastName();
+	birthDate = currentPerson.getBirthDate();
+	familyTree[firstName + ", " + lastName + ", " + birthDate] = currentPerson;
+	cout << endl << "   [2] " + currentPerson.getFirstName() + " " + currentPerson.getLastName() <<
+		" was added to the Family Tree." << endl;
 }
 
 Person createRelative() {
@@ -146,6 +145,66 @@ Person createRelative() {
 	relative.setDeathDate(deathDate);
 	relative.setGender(gender);
 
+	cout << endl << "The following steps were completed:" << endl;
+	cout << endl << "   [1] Person " + relative.getFirstName() + " " + relative.getLastName() + " was created." << endl;
 	return relative;
+}
+
+bool isPersonFound(string selectedPerson, map<string,Person>& familyTree, map<string, Person>::iterator& it) {
+	it = familyTree.find(selectedPerson);
+	if (it == familyTree.end()) 	
+		return false;
+	else 
+		return true;
+}
+
+//The person that was requested did not exist, prompt the user and ask
+//if they would like to create a new person and add it to the Family Tree
+void addRelative(map<string,Person>& familyTree) {
+	string response;
+	string firstName, lastName, birthDate;
+	Person currentPerson;
+	cout << endl << "Person does not exist!!! Would you like to create them??? (y or n):";
+	getline(cin, response);
+
+	if (response == "y" || response == "Y") {
+		currentPerson = createRelative();
+		addPersonToTree(familyTree, currentPerson);
+	} 
+}
+void connectRelative(string type, map<string,Person>& familyTree, Person& currentPerson) {
+	string choice, connection;
+	Person relative;
+	map<string, Person>::iterator it;
+
+	cout << endl << "Enter one of the following connecting methods (1 or 2):" << endl;
+	cout << "\t" << "[1] Add an existing " + type + " to " + currentPerson.getFirstName() + " " + currentPerson.getLastName() + "." << endl;
+	cout << "\t" << "[2] Create a " + type + " to add to " + currentPerson.getFirstName() + " " + currentPerson.getLastName() + "." << endl;
+	getline(cin, choice);
+	
+	if (choice == "1") {
+		cout << endl << "What " + type + " would you like to add (first name, last name, birthday)?"; 
+		getline(cin, connection);
+		if (isPersonFound(connection, familyTree, it) == true) {
+			if (type == "Mother") {
+				relative = it->second;
+				relative.addChild(currentPerson);
+				addPersonToTree(familyTree, relative);
+				currentPerson.setMother(relative);
+				addPersonToTree(familyTree, currentPerson);
+			} else if (type == "Father") {
+				relative = it->second;
+				relative.addChild(currentPerson);
+				addPersonToTree(familyTree, relative);
+				currentPerson.setFather(relative);
+				addPersonToTree(familyTree, currentPerson);
+			} else if (type == "Child") {
+				relative = it->second;
+				currentPerson.addChild(relative);
+				addPersonToTree(familyTree, currentPerson);
+			}
+
+		}
+	}
 }
 
